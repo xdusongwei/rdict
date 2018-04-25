2af6f2bd56033ea17133ac99592840a6f4d9952f
宋伟 <songwei@songwei.io>
2018-04-25 10:39:51 +0800

    mod setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,10 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     install_requires=["redis"],
 )
