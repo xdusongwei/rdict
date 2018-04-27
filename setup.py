ab275a9db079b18cd4da17110441d3407bdb106a
宋伟 <songwei@songwei.io>
2018-04-27 14:36:59 +0800

    mod setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rdict',
-    version='0.1.0',
+    version='0.1.1',
     author='songwei',
     author_email='songwei@songwei.io',
     description='A Pythonic tool is used for operating redis commands.',
@@ -11,6 +11,7 @@ setup(
     python_requires='>=3',
     packages=['rdict'],
     ext_modules=[],
+    license='MIT',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -19,6 +20,10 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
     ],
     install_requires=["redis"],
 )
